dbed82bca6dda62607e76ab8474b81b9fbcd7815
andreeamiclaus <96205415+andreeamiclaus@users.noreply.github.com>
2022-08-28 13:22:07 +0200

    Add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import Header from './components/Header'
-import Main from './components/Main'
-import Basket from './components/Basket'
-
+import Header from './components/Header';
+import Main from './components/Main';
+import Basket from './components/Basket';
+import data from './data';
+import { useState } from 'react';
 function App() {
-  const [count, setCount] = useState(0)
-
+  const { products } = data;
+  const [cartItems, setCartItems] = useState([]);
+  const onAdd = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist) {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, qty: 1 }]);
+    }
+  };
+  const onRemove = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist.qty === 1) {
+      setCartItems(cartItems.filter((x) => x.id !== product.id));
+    } else {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
+        )
+      );
+    }
+  };
   return (
     <div className="App">
-     <Header>Helllloo </Header>
-     <div>
-     <Main>Main</Main>
-     <Basket>Basket</Basket>
+      <Header countCartItems={cartItems.length}></Header>
+      <div className="row">
+        <Main products={products} onAdd={onAdd}></Main>
+        <Basket
+          cartItems={cartItems}
+          onAdd={onAdd}
+          onRemove={onRemove}
+        ></Basket>
       </div>
-      <p className="read-the-docs">
-      bl BLA BLA
-      </p>
-   
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
